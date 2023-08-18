5474a55914caa90a917dd4e8da35980187ad856e
ledatdz123 <lephuocdat0912a2@gmail.com>
2023-08-18 19:00:30 +0700

    toastify

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
-//export const BASE_API="https://media-chat-server.onrender.com"
-export const BASE_API="http://localhost:8080"
+export const BASE_API="https://media-chat-server.onrender.com"
+//export const BASE_API="http://localhost:8080"
